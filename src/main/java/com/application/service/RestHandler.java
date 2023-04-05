11cdb072bbb4c634538977c0eed986d29a4890a3
chintam anand <chintamanand56@gmail.com>
2023-04-05 23:09:39 +0530

    Removing the unWanted configurations from yaml file
    Removing the unWanted configurations from yaml file

--- src/main/java/com/application/service/RestHandler.java
+++ src/main/java/com/application/service/RestHandler.java
@@ -14,33 +14,33 @@ import java.util.List;
 @Configuration
 public class RestHandler {
 
-    private final RestTemplate restTemplate;
+    //private final RestTemplate restTemplate;
     //https://www.universal-tutorial.com/rest-apis/free-rest-api-for-country-state-city
 
-    @Value(value = "${external.api1.token}")
-    private String apiToken1;
-
-    @Value("${external.api1.url}")
-    private String apiUrl1;
-
-    public RestHandler(){
-        this.restTemplate = new RestTemplate();
-    }
-
-    public List<CityDto> getStates(String countryName) {
-        log.info("Entered the RestTemplate's getState Method -- "+ countryName);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization","Bearer " + apiToken1);
-        headers.set("user-email", "chintamanand56@gmail.com");
-        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-        log.info("api Url: " + apiUrl1+"states/"+ countryName);
-        log.info("api token: " + apiToken1);
-        log.info("headers Info: " + headers.toString());
-        ResponseEntity<String> response = new RestTemplate().exchange(apiUrl1+"states/"+ countryName, HttpMethod.GET, httpEntity, String.class);
-        log.info("Response received: "+response.getBody());
-
-        return Collections.emptyList();
-    }
+//    @Value(value = "${external.api1.token}")
+//    private String apiToken1;
+//
+//    @Value("${external.api1.url}")
+//    private String apiUrl1;
+//
+//    public RestHandler(){
+//        this.restTemplate = new RestTemplate();
+//    }
+//
+//    public List<CityDto> getStates(String countryName) {
+//        log.info("Entered the RestTemplate's getState Method -- "+ countryName);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        headers.set("Authorization","Bearer " + apiToken1);
+//        headers.set("user-email", "chintamanand56@gmail.com");
+//        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+//        log.info("api Url: " + apiUrl1+"states/"+ countryName);
+//        log.info("api token: " + apiToken1);
+//        log.info("headers Info: " + headers.toString());
+//        ResponseEntity<String> response = new RestTemplate().exchange(apiUrl1+"states/"+ countryName, HttpMethod.GET, httpEntity, String.class);
+//        log.info("Response received: "+response.getBody());
+//
+//        return Collections.emptyList();
+//    }
 
 }
